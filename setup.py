f8c2609d93cde3e72bd30058c6d52ebcbac3076b
RS4327 <131863859+RS4327@users.noreply.github.com>
2023-04-26 22:03:34 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": null,
-   "id": "44101c0d",
+   "execution_count": 2,
+   "id": "92f02dd8",
    "metadata": {},
-   "outputs": [],
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax. Perhaps you forgot a comma? (1726595812.py, line 12)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[1;36m  Cell \u001b[1;32mIn[2], line 12\u001b[1;36m\u001b[0m\n\u001b[1;33m    packages=find_packages[]\u001b[0m\n\u001b[1;37m             ^\u001b[0m\n\u001b[1;31mSyntaxError\u001b[0m\u001b[1;31m:\u001b[0m invalid syntax. Perhaps you forgot a comma?\n"
+     ]
+    }
+   ],
    "source": [
     "from setuptools import setup, find_packages\n",
     "\n",
@@ -17,8 +26,18 @@
     "    package_dir={'': 'src'},\n",
     "    packages=find_packages(where='src'),\n",
     "    python_requires='>=3.8',\n",
+    "    url=\"https://github.com/RSR4327/Face-Recognition\",\n",
+    "    packages=find_packages[]\n",
     ")"
    ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d3887338",
+   "metadata": {},
+   "outputs": [],
+   "source": []
   }
  ],
  "metadata": {
